154feefd5c6dcba88836cbdfe2d90a80639128d4
GaitanJulian <juliangaitan_h@hotmail.com>
2023-05-18 23:22:02 -0500

    Add IdleState

--- Proyecto Colombia/Assets/Scripts/Enemies/Base/EnemyStateManagerScriptableObject.cs
+++ Proyecto Colombia/Assets/Scripts/Enemies/Base/EnemyStateManagerScriptableObject.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyStateManager", menuName = "Enemy/State Manager")]
 public class EnemyStateManagerScriptableObject : ScriptableObject
 {
-    public EnemyBaseState idleState;
-    public EnemyBaseState chasingState;
-    public EnemyBaseState attackingState;
-    // Add more states as needed
+    public EnemyBaseState _idleState;
+    public EnemyBaseState _chasingState;
+    public EnemyBaseState _attackingState;
 
+    [SerializeField] private EnemyStatsScriptableObject _enemyStats;
+    
     // Any additional data or properties specific to the state manager can be defined here
 
     public EnemyBaseState GetInitialState()
     {
-        return idleState; // Return the initial state for the enemy
+        return _idleState; // Return the initial state for the enemy
     }
 }
---
+++ Proyecto Colombia/Assets/Scripts/Enemies/Base/IdleState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IdleState", menuName = "Enemy/States/Idle")]
+public class IdleState : EnemyBaseState
+{
+    public override void EnterState(EnemyController enemy)
+    {
+        
+    }
+
+    public override void UpdateState(EnemyController enemy)
+    {
+       
+    }
+}

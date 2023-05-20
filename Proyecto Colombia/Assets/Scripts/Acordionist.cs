e76f918c5353c0f5f435793d05e1c469e5316b52
Fitman22 <85381077+Fitman22@users.noreply.github.com>
2023-05-20 17:02:27 -0500

    [add]prefab_acordionist

--- Proyecto Colombia/Assets/Scripts/Acordionist.cs
+++ Proyecto Colombia/Assets/Scripts/Acordionist.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Acordionist : MonoBehaviour
 {
     [SerializeField] GameObject onda;
     [SerializeField] Transform salida;
     [SerializeField] float force, radio, timeatack, distance;
+    CharacterController movControll;
+    Vector2 direccion;
     float time;
     void Start()
     {
-        
+        movControll = gameObject.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -28,7 +31,7 @@ public class Acordionist : MonoBehaviour
     }
     void shoot()
     {
-        
+        direccion = movControll.ReturnDirection();
         StartCoroutine(wait(0.2f));
 
     }
@@ -36,11 +39,14 @@ public class Acordionist : MonoBehaviour
     {
         GameObject[] Ondas = new GameObject[3];
         float t = distance;
+        float angle = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+        angle += 220;
         for (int i = 0; i < Ondas.Length; i++)
         {
-
-            Ondas[i] = Instantiate(onda, salida.position, salida.rotation);
-            Ondas[i].GetComponent<Rigidbody2D>().velocity = new Vector2(force, 0);
+            
+            Ondas[i] = Instantiate(onda, salida.position, Quaternion.AngleAxis(angle, Vector3.forward));
+           
+            Ondas[i].GetComponent<Rigidbody2D>().velocity = direccion*force;
             Ondas[i].transform.localScale = new Vector2(radio-i*3,radio-i*3);
             Ondas[i].GetComponent<OndaDestroy>().distance = t;
             t /= 2;

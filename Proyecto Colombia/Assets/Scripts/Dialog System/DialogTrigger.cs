1f0af1943ece62c8217820f59334cc128c590efb
Julian Gaitan <juliangaitan_h@hotmail.com>
2023-05-30 21:26:47 -0500

    Add npc mockup

--- Proyecto Colombia/Assets/Scripts/Dialog System/DialogTrigger.cs
+++ Proyecto Colombia/Assets/Scripts/Dialog System/DialogTrigger.cs
@@ -3,13 +3,32 @@ using Fungus;
 
 public class DialogTrigger : MonoBehaviour
 {
-    public Flowchart flowchart;
+    [SerializeField] private Flowchart _flowchart;
+    [SerializeField] private GameObject _conversationSprite ;
+    private string PLAYERTAG = "Player";
+    private bool _canInteract = false;
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void Start()
     {
-        if (collision.CompareTag("Player"))
+        _conversationSprite.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(PLAYERTAG))
         {
-            flowchart.ExecuteBlock("TestBlock");
+            _conversationSprite.SetActive(true); // Show the thinking sprite
+            _canInteract = true;
         }
     }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _conversationSprite.SetActive(false); // Hide the thinking sprite
+            _canInteract = false;
+        }
+    }
+
 }
